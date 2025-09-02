23ab4f761049ddd19470949cb68b402484081f18
Kobayashi82 <kobayashi82@outlook.com>
2025-09-02 22:22:11 +0200

    Más limpieza

--- Taskmasterctl/inc/Taskmasterctl.hpp
+++ Taskmasterctl/inc/Taskmasterctl.hpp
@@ -1,26 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Authentication.hpp                                 :+:      :+:    :+:   */
+/*   Taskmasterctl.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vzurera- <vzurera-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/18 13:40:01 by vzurera-          #+#    #+#             */
-/*   Updated: 2025/08/18 13:46:05 by vzurera-         ###   ########.fr       */
+/*   Created: 2025/09/02 22:19:58 by vzurera-          #+#    #+#             */
+/*   Updated: 2025/09/02 22:19:58 by vzurera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-
-#pragma region "Includes"
-
-	#include <string>
-
-#pragma endregion
-
-#pragma region "Methods"
-
-	int		get_userpass(const std::string msg, std::string & user, std::string & pass);	// Extracts username and password from a message
-	bool	authenticate(const std::string& user, const std::string& pass);					// Authenticates a user with username and password
-
-#pragma endregion
